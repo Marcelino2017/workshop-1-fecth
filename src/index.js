e83cbbc9bc8ac2f7911097ed6b077044e5ff3fb1
Marcelino2017 <marcelinomebe@gmail.com>
2021-10-31 17:22:26 -0500

    Descargando información y creando nodos v1

--- src/index.js
+++ src/index.js
@@ -3,4 +3,34 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+//conectar al serve
+window.fetch(url)
+//Procesar a respuesta, y convertirla en JSON
+.then((repuesta) => repuesta.json())
+//JSON -> data -> Renderizarla en JSON
+.then((resposeJson) => {
+    const todosLosItem = [];
+    resposeJson.data.forEach(item => {
+        //console.log(item.name);
+        //crear imagen
+        const imagen = document.createElement('img');
+        
+        //crear titulo
+        const title = document.createElement('h2');
+
+        //crear price
+        const price = document.createElement('div');
+
+        const container = document.createElement('div');
+        container.append(imagen, title, price);
+
+        todosLosItem.push(container)
+        
+    });
+
+    document.body.append(todosLosItem);
+})
