079cf20d02404435914f5eb333e3a6408761bccc
Marcelino2017 <marcelinomebe@gmail.com>
2021-11-01 17:30:23 -0500

    Usando la API de internacionalización del browser

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@
 
 //console.log('Happy hacking :)')
 const baseUrl = "https://platzi-avo.vercel.app";
+
 const appNode = document.getElementById("app");
+
+// Intl (formato de intarnacionalización)
+//formato a fecha  y moneda
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-En', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+
+    return newPrice;
+}
 
 //web api
 //conectar al serve
@@ -22,20 +34,27 @@ window.fetch(`${baseUrl}/api/avo`)
         const imagen = document.createElement('img');
         //URL de la imagen
         imagen.src = `${baseUrl}${item.image}`;
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
         //crear titulo
         const title = document.createElement('h2');
-        title.textContent = item.name
+        title.textContent = item.name;
+        //title.style.fontSize = "3rem"
+        //segunda opcion 
+        title.className = 'text-lg'
 
         //crear price
         const price = document.createElement('div');
-        price.textContent = item.price;
+        price.className = "text-gray-600";
+        price.textContent = formatPrice(item.price);
 
         const container = document.createElement('div');
         container.append(imagen, title, price);
+        container.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";  
 
         todosLosItem.push(container)
         
     });
 
     appNode.append(...todosLosItem);
+    appNode.className = "mt-10 grid  grid-cols-2 gap-2"
 })
